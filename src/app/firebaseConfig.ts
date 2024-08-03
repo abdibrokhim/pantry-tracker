ddc226d7cc7b7acb7238b09b59c587b54820c1f6
abdibrokhim <abdibrokhim@gmail.com>
2024-08-03 20:15:20 +0500

    update

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -7,15 +7,15 @@
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// export const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-// };
+export const firebaseConfig = {
+  apiKey: "AIzaSyBN22xMiVt0oNEa41T_BMZcgjupJBN0RDo",
+  authDomain: "chatwithpdf-30e42.firebaseapp.com",
+  projectId: "chatwithpdf-30e42",
+  storageBucket: "chatwithpdf-30e42.appspot.com",
+  messagingSenderId: "528066992871",
+  appId: "1:528066992871:web:41f74e666abae5d3152042",
+  measurementId: "G-X4SQT3N4V",
+};
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
