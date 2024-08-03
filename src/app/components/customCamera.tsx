eacc564ff5ae88509d347183915b669477b54e79
abdibrokhim <abdibrokhim@gmail.com>
2024-08-03 20:05:32 +0500

    update

--- src/app/components/customCamera.tsx
+++ src/app/components/customCamera.tsx
@@ -8,15 +8,26 @@ import { initializeApp } from 'firebase/app';
 import { getStorage, ref, uploadString, getDownloadURL } from 'firebase/storage';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import OpenAI from 'openai';
-import { firebaseConfig } from '../firebaseConfig';
+// import { firebaseConfig } from '../firebaseConfig';
 import { Food } from './types';
+
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const firestore = getFirestore(app);
 
-const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY!, dangerouslyAllowBrowser: true});
+const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, dangerouslyAllowBrowser: true});
+
 
 export default function CameraComponent() {
   const camera = useRef<CameraType>(null);
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,20 @@
 import React, { useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { firebaseConfig } from "../firebaseConfig";
+// import { firebaseConfig } from "../firebaseConfig";
 import { Button, Box } from "@mui/material";
 import { Google as GoogleIcon } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
+
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
